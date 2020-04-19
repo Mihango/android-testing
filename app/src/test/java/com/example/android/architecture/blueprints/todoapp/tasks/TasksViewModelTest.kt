2b4aa8d4cc6e1fa4cb06d0d71d09a717bf5407c5
Mihango <mihangokennedy@gmail.com>
2020-04-19 18:34:27 +0300

    added setup with Before

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,7 +1,12 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.*
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
@@ -9,13 +14,35 @@ import org.robolectric.annotation.Config
 @Config(manifest= Config.NONE)
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var taskViewModel: TasksViewModel
+
+    @Before
+    fun setupViewModel() {
+        taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
 
     @Test
     fun addNewTask_setsNewTaskEvent() {
         // Given a fresh TaskViewModel
-        val taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
         // When adding a new task
         taskViewModel.addNewTask()
         // Then the new task event is triggered
+        val value = taskViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(value.getContentIfNotHandled(), (not(nullValue())))
+    }
+
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible() {
+        // Given a fresh ViewModel
+
+        // When the filter is ALL_TASKS
+        taskViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+        // Then the Add task action is visible
+        val value = taskViewModel.tasksAddViewVisible.getOrAwaitValue()
+        assertThat(value, `is`(true))
     }
 }
