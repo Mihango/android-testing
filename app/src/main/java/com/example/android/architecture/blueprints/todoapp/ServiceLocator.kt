10368349119842316b8106484b80acd231a97e3f
Mihango <mihangokennedy@gmail.com>
2020-04-22 18:56:51 +0300

    updated service locator for reseting during tests

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
@@ -1,6 +1,7 @@
 package com.example.android.architecture.blueprints.todoapp
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
 import androidx.room.Room
 import com.example.android.architecture.blueprints.todoapp.data.source.DefaultTasksRepository
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksDataSource
@@ -8,16 +9,34 @@ import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepo
 import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksLocalDataSource
 import com.example.android.architecture.blueprints.todoapp.data.source.local.ToDoDatabase
 import com.example.android.architecture.blueprints.todoapp.data.source.remote.TasksRemoteDataSource
+import kotlinx.coroutines.runBlocking
 
+@VisibleForTesting
 object ServiceLocator {
     private var database: ToDoDatabase? = null
+    private val lock = Any()
 
     @Volatile
     var tasksRepository: TasksRepository? = null
+        @VisibleForTesting set
 
     fun provideTasksRepository(context: Context): TasksRepository {
         synchronized(this) {
             return tasksRepository ?: createTasksRepository(context)
+        }
+    }
+
+    @VisibleForTesting
+    fun resetRepository() {
+        synchronized(lock) {
+            runBlocking { TasksRemoteDataSource.deleteAllTasks() }
+            // clear all data to avoid test pollution
+            database?.apply {
+                clearAllTables()
+                close()
+            }
+            database = null
+            tasksRepository = null
         }
     }
 
